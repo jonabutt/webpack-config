cf435229a09c1c52f31ac286b35029f9b63ba037
jonabutt <jonathanbuttigieg1@gmail.com>
2021-12-26 17:23:38 +0100

    added browserlist to be used by postcss and webpack, webpack fix hotreload

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let mode = "development";
+let target = "web";
 
 if(process.env.NODE_ENV === "production"){
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
     module: {
         rules: [
             {
-                test: /\.s?css$/i,
+                test: /\.(s[ac]|c)ss$/i,
                 use: [MiniCssExtractPlugin.loader,"css-loader","postcss-loader","sass-loader"]
             },
             {
